6f0f76ec66dd91a97459952217e8cbb7cba21b69
Marcos Gonzalvez <marcosgondesign@gmail.com>
2025-02-13 00:56:37 -0300

    corrigiendo backend 4

--- models/newProductModel.js
+++ models/newProductModel.js
@@ -2,8 +2,8 @@ const { pool } = require("../server/server");
 
 
 const createProduct = async (user_id, title, description, price, image_url, stock) => {
-    const consulta = 'INSERT INTO products (user_id, title, description, price, image_url, stock) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *';       
-    const values = [user_id, title, description, price, image_url, stock];
+    const consulta = 'INSERT INTO products (user_id, category_id, title, description, price, image_url, stock) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING *';       
+    const values = [user_id, category_id, title, description, price, image_url, stock];
 
     const { rows, rowCount } = await pool.query(consulta, values);
     return { rows, rowCount };
