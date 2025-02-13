cc7d38b3cafbf58303696d7bea887d3431e2477d
Marcos Gonzalvez <marcosgondesign@gmail.com>
2025-02-13 00:52:23 -0300

    corrigiendo backend 4

--- controllers/newProductController.js
+++ controllers/newProductController.js
@@ -2,11 +2,11 @@ const ProductModel = require('../models/newProductModel');
 const ProductView = require('../views/newProductView');
 
 const createProduct = async (req, res) => {
-    const { category_id, title, description, price, image_url, stock } = req.body;
+    const { title, description, price, image_url, stock } = req.body;
     const user_id = req.user.id; // Obtener el ID del usuario autenticado
 
     try {
-        const { rows, rowCount } = await ProductModel.createProduct(user_id, category_id, title, description, price, image_url, stock);
+        const { rows, rowCount } = await ProductModel.createProduct(user_id, title, description, price, image_url, stock);
         ProductView.successResponse(res, rows, rowCount);
     } catch (error) {
         console.error(error);
