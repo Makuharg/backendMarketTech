dc2aa53df01ddf6c8fa1e432671dfdb527285ada
Marcos Gonzalvez <marcosgondesign@gmail.com>
2025-02-20 20:57:45 -0300

    modificando transactions

--- models/getTransactionModel.js
+++ models/getTransactionModel.js
@@ -13,8 +13,10 @@ const getTransactions = async (user_id, total_price) => {
             transactions t
         JOIN 
             users u_buyer ON t.user_id = u_buyer.id
+        JOIN 
+            users u_seller ON t.seller_id = u_seller.id
         WHERE 
-            t.user_id = $1`;
+            t.user_id = $1 OR t.seller_id = $2`;
 
     const values = [user_id, total_price];
 
