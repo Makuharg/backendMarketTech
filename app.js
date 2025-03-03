f7a2000778df407f4b0b8cfe46add5502985c291
Marcos Gonzalvez <marcosgondesign@gmail.com>
2025-03-03 20:48:42 -0300

    agregando carga de imagenes

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const upload = multer({ dest : 'uploads' })
 
 app.post('/api/user/files/upload', upload.single('image'), async(req,res)=>{
     try {
-        const result = await cloudinary.uploader.upload(req.image.path);
+        const result = await cloudinary.uploader.upload(req.file.path);
         res.status(200).json(result.url);
 
     } catch (error) {
